88bbda08e002abcbf85074d50532ffb1ae1584ec
Nakell Williams <nakellw@yahoo.com>
2018-10-01 18:32:55 -0400

    still script error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -36,8 +36,9 @@ module.exports = function(app) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
-    
-      tableData.push(req.body);
+    console.log(req);
+    console.log(friendData);
+      friendData.push(req.body);
       res.json(true);
   });
 
